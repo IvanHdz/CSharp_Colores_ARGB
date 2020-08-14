2e0a43e8bc7508b5f8b6523ec3f1462ebc4d5afc
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-08-14 12:19:13 -0500

    Commit

---
+++ Convertidor.Colores/Colores.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace Convertidor.Colores
+{
+    public class Colores
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public void ToArgb(string colorcode)
+        {
+            int argb = Int32.Parse(colorcode.Replace("#", ""), NumberStyles.AllowHexSpecifier);
+
+            Console.Write("El valor en ARGB es: {0} \n", argb);
+
+            FromArgb(argb);
+            IntToColor(argb);
+            IntToColor2();
+            IntToColor3("Brown");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public void FromArgb(int argb)
+        {
+            System.Drawing.Color clr = System.Drawing.Color.FromArgb(argb);
+
+            Console.Write("El valor de ARGB a System.Drawing.Color es: {0} \n", clr.ToString());
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private void IntToColor(int argb)
+        {
+            System.Drawing.Color x = System.Drawing.Color.FromArgb(argb);
+
+            var solid = new SolidColorBrush(Color.FromArgb(x.A, x.R, x.G, x.B));
+
+            Console.Write("El valor de ARGB a SolidColorBrush es: {0} \n", solid.ToString());
+        }
+
+        private void IntToColor2()
+        {
+            var tt = (SolidColorBrush)new BrushConverter().ConvertFromString("Brown");
+
+            Console.Write("\n **************** \n\n");
+            Console.Write("El valor de Café a SolidColorBrush es: {0} \n", tt.ToString());
+        }
+
+        private void IntToColor3(string color)
+        {
+            var converter = new BrushConverter();
+            var brush = (Brush)converter.ConvertFromString(color);
+
+            Console.Write("El valor en Brush es: {0}", brush.ToString());
+        }
+    }
+}
---
+++ Convertidor.Colores/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Convertidor.Colores
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //string colorcode = "#FF0000FF";
+            string colorcode = "#FF330066";
+
+            Console.WriteLine("Valor Hexadecimal es:" + colorcode + "\n");
+
+            Colores color = new Colores();
+            color.ToArgb(colorcode);
+
+            Console.Read();
+        }
+    }
+}
